6b64042d2394589662814f3d8639ec9705c57aea
PlainMyth <stomeyer4@gmail.com>
2024-12-10 15:07:56 -0800

    Added lighting onto sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,7 +4,7 @@ import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link, NavLink } from 'react-router-dom'
 import {faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
-
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 const Sidebar = () => {
     return (
         <div className='nav-bar'>
@@ -13,16 +13,39 @@ const Sidebar = () => {
                 <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
             </Link>
             <nav>
-                <NavLink exact="true" activeclassname="active" to="/">
+                <NavLink 
+                exact="true" 
+                activeclassname="active" 
+                to="/">
                     <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <NavLink 
+                exact="true" 
+                activeclassname="active" 
+                className="about-link" 
+                to="/about">
                     <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <NavLink 
+                exact="true" 
+                activeclassname="active" 
+                className="contact-link" 
+                to="/contact">
                     <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
                 </NavLink>
             </nav>
+            <ul>
+                <li>
+                    <a target = "_blank" rel = 'noreferrer' href ='https://www.linkedin.com/in/sean-le-7b1770237/'>
+                        <FontAwesomeIcon icon= { faLinkedin } color = "#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a target = "_blank" rel = 'noreferrer' href ='https://github.com/PlainMyth'>
+                        <FontAwesomeIcon icon= { faGithub } color = "#4d4d4e" />
+                    </a>
+                </li>
+            </ul>
         </div>
     )
 }
